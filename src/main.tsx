242801203648d5127aff0a465e5ba768c738179c
lwmraymond <49761462+lwmraymond@users.noreply.github.com>
2026-07-27 11:14:00 +0800

    style: load ITSM capability styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles-differentiated.css';
 import './styles-polish.css';
 import './styles-round2.css';
 import './styles-kibana-fidelity.css';
+import './styles-itsm-capabilities.css';
 import { App } from './App';
 import { AppErrorBoundary } from './AppErrorBoundary';
 import { PlatformThemeProvider } from './theme';
